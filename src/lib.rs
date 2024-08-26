3eb7268cecca7256d1b398d2f0c683a1eae0e3f6
Samuel Sharp <samuelsharp4@gmail.com>
2024-08-26 09:11:34 -0700

    Basic trip stops listing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use gtfs_realtime::{FeedEntity, FeedHeader, FeedMessage, VehiclePosition};
+use gtfs_realtime::trip_update::{StopTimeEvent, StopTimeUpdate};
+use gtfs_realtime::{FeedEntity, FeedMessage};
 use inline_colorization::*;
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::error::Error;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn capitalize(s: &str) -> String {
@@ -72,9 +72,13 @@ pub async fn train_feed(
     client: &reqwest::Client,
     key: &str,
     trips: &str,
+    stoptimes: &str,
 ) -> Result<ChicagoResults, Box<dyn std::error::Error + Sync + Send>> {
     let mut trip_data_raw = csv::Reader::from_reader(trips.as_bytes());
     let trip_data = trip_data_raw.records();
+
+    let mut stop_times_raw = csv::Reader::from_reader(stoptimes.as_bytes());
+    let stop_times = stop_times_raw.records();
 
     let mut run_ids: HashMap<u16, String> = HashMap::new();
 
@@ -87,6 +91,42 @@ pub async fn train_feed(
             run_ids.insert(final_run.parse::<u16>().unwrap(), final_trip);
         }
     }
+
+    let mut trip_stops: HashMap<String, Vec<StopTimeUpdate>> = HashMap::new();
+
+    for record in stop_times {
+        let record = record?;
+        if record[3].starts_with("3") && record[3].chars().count() == 5 {
+            let record_trip = &record[0];
+
+            let record_update = StopTimeUpdate {
+                stop_sequence: Some(record[4].parse::<u32>().unwrap()),
+                stop_id: Some(record[3].to_string()),
+                arrival: Some(StopTimeEvent {
+                    delay: None,
+                    uncertainty: None,
+                    // time: Some(record[1].parse::<i64>().unwrap())
+                    time: None,
+                }),
+                departure: Some(StopTimeEvent {
+                    delay: None,
+                    uncertainty: None,
+                    // time: Some(record[2].parse::<i64>().unwrap())
+                    time: None,
+                }),
+                departure_occupancy_status: None,
+                schedule_relationship: None,
+                stop_time_properties: None,
+            };
+
+            trip_stops
+                .entry(record_trip.to_string())
+                .or_insert(vec![record_update.clone()])
+                .push(record_update);
+        }
+    }
+
+    // trip_stops.iter().for_each(|(k, v)| println!("Trip {} calls at {}, {}, {}, and more stops", k, v.get(0).unwrap().stop_id.as_ref().unwrap().to_string(), v.get(1).unwrap().stop_id.as_ref().unwrap().to_string(), v.get(2).unwrap().stop_id.as_ref().unwrap().to_string() ));
 
     let response = client
         .get("https://www.transitchicago.com/api/1.0/ttpositions.aspx")
@@ -113,6 +153,7 @@ pub async fn train_feed(
     //Vec<TTPosTrain> or TTPosTrain
 
     let mut train_positions: Vec<FeedEntity> = vec![];
+    let mut trip_updates: Vec<FeedEntity> = vec![];
 
     for train_line_group in json_output.ctatt.route {
         if let Some(train_value) = train_line_group.train {
@@ -139,59 +180,102 @@ pub async fn train_feed(
                 let train_run_id = train.rn.parse::<u16>().unwrap();
                 let train_trip_id = run_ids.get(&train_run_id);
 
-                if let Ok(lat) = lat {
-                    if let Ok(lon) = lon {
-                        let entity: FeedEntity = FeedEntity {
-                            id: train.rn.clone(),
-                            stop: None,
-                            trip_modifications: None,
-                            is_deleted: None,
-                            trip_update: None,
-                            vehicle: Some(gtfs_realtime::VehiclePosition {
-                                trip: Some(gtfs_realtime::TripDescriptor {
-                                    modified_trip: None,
-                                    trip_id: train_trip_id.cloned(),
-                                    route_id: Some(capitalize(&train_line_group.route_name)),
-                                    direction_id: Some(train.tr_dr.parse::<u32>().unwrap()),
-                                    start_time: None,
-                                    start_date: None,
-                                    schedule_relationship: None,
+                if train_trip_id.is_some() {
+                    if let Ok(lat) = lat {
+                        if let Ok(lon) = lon {
+                            let pos_entity: FeedEntity = FeedEntity {
+                                id: train.rn.clone(),
+                                stop: None,
+                                trip_modifications: None,
+                                is_deleted: None,
+                                trip_update: None,
+                                vehicle: Some(gtfs_realtime::VehiclePosition {
+                                    trip: Some(gtfs_realtime::TripDescriptor {
+                                        modified_trip: None,
+                                        trip_id: train_trip_id.cloned(),
+                                        route_id: Some(capitalize(&train_line_group.route_name)),
+                                        direction_id: Some(train.tr_dr.parse::<u32>().unwrap()),
+                                        start_time: None,
+                                        start_date: None,
+                                        schedule_relationship: None,
+                                    }),
+                                    vehicle: Some(gtfs_realtime::VehicleDescriptor {
+                                        id: Some(train.rn.clone()),
+                                        label: None,
+                                        license_plate: None,
+                                        wheelchair_accessible: None,
+                                    }),
+                                    position: Some(gtfs_realtime::Position {
+                                        latitude: lat,
+                                        longitude: lon,
+                                        bearing: match train.heading.parse::<f32>() {
+                                            Ok(bearing) => Some(bearing),
+                                            _ => None,
+                                        },
+                                        odometer: None,
+                                        speed: None,
+                                    }),
+                                    current_status: None,
+                                    current_stop_sequence: None,
+                                    stop_id: None,
+                                    timestamp: Some(
+                                        SystemTime::now()
+                                            .duration_since(UNIX_EPOCH)
+                                            .expect("Time went backwards")
+                                            .as_secs(),
+                                    ),
+                                    congestion_level: None,
+                                    occupancy_percentage: None,
+                                    occupancy_status: None,
+                                    multi_carriage_details: vec![],
                                 }),
-                                vehicle: Some(gtfs_realtime::VehicleDescriptor {
-                                    id: Some(train.rn.clone()),
-                                    label: Some(train.rn.clone()),
-                                    license_plate: None,
-                                    wheelchair_accessible: None,
-                                }),
-                                position: Some(gtfs_realtime::Position {
-                                    latitude: lat,
-                                    longitude: lon,
-                                    bearing: match train.heading.parse::<f32>() {
-                                        Ok(bearing) => Some(bearing),
-                                        _ => None,
-                                    },
-                                    odometer: None,
-                                    speed: None,
-                                }),
-                                current_status: None,
-                                current_stop_sequence: None,
-                                stop_id: None,
-                                timestamp: Some(
-                                    SystemTime::now()
-                                        .duration_since(UNIX_EPOCH)
-                                        .expect("Time went backwards")
-                                        .as_secs(),
-                                ),
-                                congestion_level: None,
-                                occupancy_percentage: None,
-                                occupancy_status: None,
-                                multi_carriage_details: vec![],
-                            }),
-                            alert: None,
-                            shape: None,
-                        };
+                                alert: None,
+                                shape: None,
+                            };
 
-                        train_positions.push(entity);
+                            train_positions.push(pos_entity);
+
+                            let trip_entity: FeedEntity = FeedEntity {
+                                id: train.rn.clone(),
+                                vehicle: None,
+                                alert: None,
+                                shape: None,
+                                stop: None,
+                                trip_modifications: None,
+                                is_deleted: None,
+                                trip_update: Some(gtfs_realtime::TripUpdate {
+                                    trip: (gtfs_realtime::TripDescriptor {
+                                        modified_trip: None,
+                                        trip_id: train_trip_id.cloned(),
+                                        route_id: Some(capitalize(&train_line_group.route_name)),
+                                        direction_id: Some(train.tr_dr.parse::<u32>().unwrap()),
+                                        start_time: None,
+                                        start_date: None,
+                                        schedule_relationship: None,
+                                    }),
+                                    vehicle: Some(gtfs_realtime::VehicleDescriptor {
+                                        id: Some(train.rn.clone()),
+                                        label: None,
+                                        license_plate: None,
+                                        wheelchair_accessible: None,
+                                    }),
+                                    stop_time_update: trip_stops
+                                        .get(train_trip_id.expect("Trip ID matching failed"))
+                                        .expect("Trip not found")
+                                        .clone(),
+                                    timestamp: Some(
+                                        SystemTime::now()
+                                            .duration_since(UNIX_EPOCH)
+                                            .expect("Time went backwards")
+                                            .as_secs(),
+                                    ),
+                                    delay: None,
+                                    trip_properties: None,
+                                }),
+                            };
+
+                            trip_updates.push(trip_entity);
+                        }
                     }
                 }
             }
@@ -213,7 +297,7 @@ pub async fn train_feed(
             },
         },
         trip_updates: gtfs_realtime::FeedMessage {
-            entity: vec![],
+            entity: trip_updates,
             header: gtfs_realtime::FeedHeader {
                 timestamp: Some(
                     SystemTime::now()
@@ -237,48 +321,22 @@ mod tests {
 
     #[tokio::test]
     async fn test_train_feed() {
-        // let trips_file_data = fs::read_to_string("static/trips.txt");
+        let trips_file_data = fs::read_to_string("static/trips.txt");
+        let stoptimes_file_data = fs::read_to_string("static/stop_times.txt");
 
-        println!("Downloading schedule");
-
-        let chicago_gtfs = "https://www.transitchicago.com/downloads/sch_data/google_transit.zip";
-
-        let client = reqwest::ClientBuilder::new()
-            .use_rustls_tls()
-            .deflate(true)
-            .gzip(true)
-            .brotli(true)
-            .build()
-            .unwrap();
-
-        let response = client
-            .get(chicago_gtfs)
-            .send()
-            .await
-            .expect("download failed");
-        let bytes = response.bytes().await.expect("Could not parse bytes");
-
-        println!("extracting");
-
-        // Create a ZIP archive from the bytes
-        let mut archive = ZipArchive::new(io::Cursor::new(bytes)).unwrap();
-
-        // Find and open the desired file
-        let mut trips_file = archive
-            .by_name("trips.txt")
-            .expect("trips.txt doesn't exist");
-        let mut buffer = Vec::new();
-        io::copy(&mut trips_file, &mut buffer).unwrap();
-
-        // Convert the buffer to a string
-        let trips_content = String::from_utf8(buffer).unwrap();
-
-        println!("computing trips...");
-
-        //give to train feeds now
-
-        let train_feeds =
-            train_feed(&client, "13f685e4b9054545b19470556103ec73", &trips_content).await;
+        let train_feeds = train_feed(
+            &reqwest::ClientBuilder::new()
+                .use_rustls_tls()
+                .deflate(true)
+                .gzip(true)
+                .brotli(true)
+                .build()
+                .unwrap(),
+            "13f685e4b9054545b19470556103ec73",
+            &trips_file_data.expect("Bad trips file"),
+            &stoptimes_file_data.expect("Bad stoptimes file"),
+        )
+        .await;
 
         assert!(train_feeds.is_ok());
 
